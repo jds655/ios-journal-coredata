96d82dfa6e9366e412a445ed16b2a30f35c94daa
Joshua Sharp <sharp.josh@gmail.com>
2019-09-18 21:41:03 -0400

    PUTing to FB works, GETing from FB Works, added PUTing new local only to FB, testing

--- Journal/Journal/Model Controllers/EntryController.swift
+++ Journal/Journal/Model Controllers/EntryController.swift
@@ -22,6 +22,10 @@ protocol EntryDataDelegate {
 
 class EntryController {
     
+    init() {
+        fetchEntriesFromServer()
+    }
+    
 //    var entries: [Entry] {
 //        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
 //
@@ -58,7 +62,7 @@ class EntryController {
     
     //MARK: - Firebase code
     
-    func put(entry: Entry, completion: @escaping () -> Void = { }) {
+    func put(entry: Entry, completion: @escaping (_ error: Error?) -> Void = { _ in }) {
         let identifier = entry.identifier ?? UUID()
         entry.identifier = identifier
         let requestURL = baseURL.appendingPathComponent(identifier.uuidString).appendingPathExtension("json")
@@ -67,34 +71,39 @@ class EntryController {
         do {
             guard let entryRepresentation = entry.entryRepresentation else {
                 NSLog("Task Representation is nil")
-                completion()
+                completion(nil)
                 return
             }
             request.httpBody = try JSONEncoder().encode(entryRepresentation)
         } catch {
             NSLog("Error encoding task respresentation: \(error)")
-            completion()
+            completion(error)
             return
         }
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 NSLog("Error PUTing entry: \(error)")
-                completion()
+                completion(error)
                 return
             }
-        }
+        }.resume()
     }
     
-    func fetchEntriesFromServer(completion: @escaping () -> Void = { }) {
+    func fetchEntriesFromServer(completion: @escaping (_ error: Error?) -> Void = { _ in }) {
         let requestURL = baseURL.appendingPathExtension("json")
         URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode != 200 {
+                    print ("HTTP Response: \(response)")
+                }
+            }
             if let error = error {
                 NSLog("Error fetching entries: \(error)")
-                completion()
+                completion(error)
             }
             guard let data = data else {
                 NSLog("No data returned from entries")
-                completion()
+                completion(error)
                 return
             }
             let decoder = JSONDecoder()
@@ -103,14 +112,17 @@ class EntryController {
                 self.updateEntries(with: entryRepresentations)
             } catch {
                 NSLog("Error decoding: \(error)")
+                completion(error)
             }
             }.resume()
+            completion(nil)
     }
     
     private func updateEntries(with representations: [EntryRepresentation]) {
         let identifiersToFetch = representations.compactMap({UUID(uuidString: $0.identifier)})
         let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
         var entriesToCreate = representationsByID
+        //Update Local store with Firebase
         do {
             let context = CoreDataStack.shared.mainContext
             let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
@@ -130,7 +142,20 @@ class EntryController {
             }
             CoreDataStack.shared.saveToPersistentStore()
         } catch {
-            NSLog("Error fatching tasks from persistent store: \(error)")
+            NSLog("Error fatching entries from persistent store: \(error)")
+        }
+        //Update Firebase with local only entries
+        do {
+            let context = CoreDataStack.shared.mainContext
+            let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "NOT (identifier IN %0)", identifiersToFetch)
+            let newLocalEntries = try context.fetch(fetchRequest)
+            print ("New local entries to Firebase: \(newLocalEntries.count)")
+            for entry in newLocalEntries {
+                put(entry: entry)
+            }
+        } catch {
+            NSLog("Error putting enteties to Firebase: \(error)")
         }
     }
 }
